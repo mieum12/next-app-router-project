6ff70ea6bbda1c539b76b00ede14245698f47b51
mieum12 <qpdlqltb1215@gmail.com>
2024-01-31 19:19:32 +0900

    feat: 이미지 최적화하기

--- components/main-header.js
+++ components/main-header.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png"
 import classes from './main-header.module.css'
+import Image from "next/image";
 
 export default function MainHeader() {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href='/'>
-        <img src={logoImg.src} alt='음식사진'/>
+        {/*
+        img 태그를 대신한 Image: 이미지 최적화하기
+        png가 아닌 wenp로 온다
+        */}
+        <Image src={logoImg.src} alt='음식사진' width={100} height={100} priority/>
         NextLevel Food
       </Link>
 
