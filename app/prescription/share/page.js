a64071500e82e873b5942ad5366701ad7fc5001c
mieum12 <qpdlqltb1215@gmail.com>
2024-02-01 22:46:05 +0900

    feat: Server Action 컴포넌트로 따로 분리하기

--- app/prescription/share/page.js
+++ app/prescription/share/page.js
@@ -1,26 +1,8 @@
 import classes from './page.module.css'
 import ImagePicker from "@/components/prescription/image-picker";
+import {submitForm} from "@/lib/action";
 export default function SharePrescriptionPage() {
-  // Next에서는 onSumbit같은 것으로 직접 form을 제어하지 않는다
-  // Server Action 생성해서 form 제출을 제어하기
-  // 모인 정보들이 자동으로 formData로 수집된다
-  // -> 자스에서 제공되는 FormData class로 들어간다
-  async function submitForm(formData) {
-    'use server' //server action 생성, 서버에서만 작동하도록 보장하는 기능
 
-    // form 객체 만들기
-    const prescription = {
-      title: formData.get('title'),
-      summary: formData.get('summary'),
-      instruction: formData.get('instruction'),
-      image: formData.get('image'),
-      creator: formData.get('name'),
-      creator_email: formData.get('email')
-    }
-
-    //데이터베이스에 저장
-    console.log(prescription) //확인
-  }
 
   return (
     <>
