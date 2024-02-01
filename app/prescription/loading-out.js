9541fca273598b1797bfc1a3b4b61e5f91dee821
mieum12 <qpdlqltb1215@gmail.com>
2024-02-01 16:56:36 +0900

    feat: data fetching 부분을 분리된 컴포넌트로 아웃소싱

--- app/prescription/loading-out.js
+++ app/prescription/loading-out.js
@@ -1,4 +1,4 @@
-import classes from "./loading.module.css";
+import classes from "./loading-out.module.css";
 export default function LoadingPage() {
   return (
     <p className={classes.loading}>
--- app/prescription/page.js
+++ app/prescription/page.js
@@ -2,6 +2,16 @@ import classes from "./page.module.css";
 import Link from "next/link";
 import PrescriptionGrid from "@/components/prescription/prescription-grid";
 import {getPrescriptions} from "@/lib/prescription";
+import {Suspense} from "react";
+import LoadingPage from "@/app/prescription/loading-out";
+
+// 데이터를 가져오는 부분을 분리된 컴포넌트로 아웃소싱
+// 아래에 Suspense로 감싸준다
+// suspense는 데이터가 올때까지 로딩상태를 처리하고 대체 컨텐츠(fallback)를 표시할 수 있음
+async function Prescription() {
+  const prescription = await getPrescriptions()
+  return <PrescriptionGrid prescriptions={prescription}/>
+}
 
 export default async function PrescriptionPage() {
   // Next.js에서 useEffect나 fetch 없이 데이터를 가져오는 방법
@@ -21,7 +31,10 @@ export default async function PrescriptionPage() {
       </p>
     </header>
     <main className={classes.main}>
-      <PrescriptionGrid prescriptions={prescriptions}/>
+      <Suspense fallback={<LoadingPage/>}>
+        <Prescription/>
+      </Suspense>
+
     </main>
     </>
   )
