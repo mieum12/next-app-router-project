3d4f03a8f327502fa9d909c5b4825a092b528cee
mieum12 <qpdlqltb1215@gmail.com>
2024-02-01 16:57:46 +0900

    feat: error handle 추가

---
+++ app/prescription/error.js
@@ -0,0 +1,12 @@
+'use client'
+// 에러 컴포넌트는 반드시 클라이어언트 컴포넌트여야한다
+// Nextjs는 클라이언트 측에서 발생한 에러를 잡기 때문이다
+// 즉, 서버에서 페이지가 랜더링 된 후에 에러를 잡음
+export default function Error() {
+  return (
+    <main className='error'>
+      <h1>ERROR!!!!!!</h1>
+      <p>데이터 연결에 실패했습니다! 다시 시도해주세요.</p>
+    </main>
+  )
+}
