38ae8b139d49a5f2fac18fdd0a957d2fc18706ec
mieum12 <qpdlqltb1215@gmail.com>
2024-02-03 16:19:09 +0900

    feat: 서버측의 유효성 검사, 서버 액션에서 response 반환 및 에러 메세지 띄우기, useFormState를 이용해 response 다루기

--- app/prescription/share/page.js
+++ app/prescription/share/page.js
@@ -1,9 +1,17 @@
+'use client'
+
 import classes from './page.module.css'
 import ImagePicker from "@/components/prescription/image-picker";
 import {submitForm} from "@/lib/action";
 import PrescriptionFormSubmitButton from "@/components/prescription/prescription-form-submit";
+import {useFormState} from 'react-dom'
 export default function SharePrescriptionPage() {
 
+  // useFormState은 form을 사용하는 페이지나 컴포넌트의 상태를 관리
+  // 2개의 인자: form이 제출될때 실행되는 실제 server action, 초기값
+  // submitForm 대신 formAction을 써준다
+  // 결국 상태는 submitForm의 응답과 실행에 따라 변경된다
+  const [state, formAction] = useFormState(submitForm, {message: null})
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +21,7 @@ export default function SharePrescriptionPage() {
         <p>Or any other prescription you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={submitForm}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,6 +50,8 @@ export default function SharePrescriptionPage() {
             ></textarea>
           </p>
           <ImagePicker label='user image' name='image'/>
+          {/*에러메세지 출력*/}
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <PrescriptionFormSubmitButton/>
           </p>
